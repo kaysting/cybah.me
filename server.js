1c7ca139d5e5ccc1e6cc3ba9c8bb2d7265487d40
CyberGen49 <kayla@cybah.me>
2025-03-27 08:28:51 +0000

    Use WIP file index

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const path = require('path');
 const express = require('express');
 const logger = require('cyber-express-logger');
+const index = require('/home/kayla/node/express-file-index');
 
 const sanitizeStringForUrl = (string) => {
     return string.replace(/ /gi, '-').replace(/[^a-z0-9-]/gi, '').toLowerCase();
 };
 
 const port = 8255;
-const srv = express();
+const app = express();
 
-srv.use(logger({
+app.use(logger({
     getIP: req => req.headers['cf-connecting-ip']
 }));
 
-srv.use((req, res, next) => {
+app.use((req, res, next) => {
     if (req.headers.host == 'simplecyber.org') {
         return res.redirect('https://cybah.me' + req.originalUrl);
     }
     next();
 });
 
-srv.use(express.static(path.join(__dirname, 'web')));
+app.use(index({
+    rootDir: path.join(__dirname, 'web'),
+    serverName: 'Cybah.me'
+}));
 
-srv.get(`/repo/:name`, (req, res) => {
+app.get(`/repo/:name`, (req, res) => {
     res.redirect(`https://github.com/CyberGen49/${req.params.name}`);
 });
 
@@ -31,10 +35,10 @@ for (const account of accounts) {
     if (!account.href) continue;
     if (!account.href.startsWith('http')) continue;
     const slug = sanitizeStringForUrl(account.slug || account.site);
-    srv.get(`/${slug}`, (req, res) => {
+    app.get(`/${slug}`, (req, res) => {
         res.redirect(account.href);
     });
     console.log(`Account short link: /${slug} -> ${account.href}`);
 }
 
-srv.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(port, () => console.log(`Listening on ${port}`));
